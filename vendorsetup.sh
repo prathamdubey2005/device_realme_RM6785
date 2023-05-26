ebc638ecd180128ff46c981ab19291ecf3da5048
SamarV-121 <samarvispute121@pm.me>
2023-05-26 21:38:26 +0530

    RM6785: vendorsetup: Import all of the recommended patches
    Import and use all of the recommended patches as per the organization's local manifest.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,15 @@ function apply_patch() {
 
 # Media: Import codecs/omx changes from t-alps-q0.mp1-V9.122.1
 apply_patch "frameworks/av" "https://github.com/ArrowOS/android_frameworks_av/commit/1fb1c48309cf01deb9e3f8253cb7fa5961c25595.patch"
+
+# BrightnessUtils: Fix the brightness slider curve for some devices
+apply_patch "frameworks/base" "https://github.com/realme-mt6785-devs/android_frameworks_base/commit/7d626a51c37bf40dcceeae0c52afc4b5fbf5203a.patch"
+
+# Biometrics: Allow the disabling of fingerprint cleanups
+apply_patch "frameworks/base" "https://github.com/AOSP-XIII/frameworks_base/commit/ada64b488725c66f948221a3b9403f2b5f040a43.patch"
+
+# AuthService: Add support for workaround side fps props
+apply_patch "frameworks/base" "https://github.com/AOSP-XIII/frameworks_base/commit/cc8546c540f742c62e470129abb1eb040948dc1d.patch"
+
+# gd: hci: Ignore unexpected status events
+apply_patch "packages/modules/Bluetooth" "https://github.com/LineageOS/android_packages_modules_Bluetooth/commit/e7f12ea3dbf6dc632f38ecb75a406c64e90a3f34.patch"
