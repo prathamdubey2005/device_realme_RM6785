1682f40dcb8ae15a28be1dc769e1bdab86c1c2ad
7Soldier <reg.fm4@gmail.com>
2023-05-26 21:23:07 +0530

    RM6785: vendorsetup: Import codec improvements
    Improve the codecs by importing these changes for better media playback and to fix low-quality playback for some content.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,6 @@ function apply_patch() {
         echo -e "\nERROR: Failed to patch the '$1' directory...\n"
     fi
 }
+
+# Media: Import codecs/omx changes from t-alps-q0.mp1-V9.122.1
+apply_patch "frameworks/av" "https://github.com/ArrowOS/android_frameworks_av/commit/1fb1c48309cf01deb9e3f8253cb7fa5961c25595.patch"
